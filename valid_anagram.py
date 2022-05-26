f4dac6c08e1a7d2a6abd7815dbbd0b28e0cbf36b
Sadat Issah <51789921+sadatyussuf@users.noreply.github.com>
2022-05-26 16:12:56 +0000

    correction made to valid_anagram.py

--- valid_anagram.py
+++ valid_anagram.py
@@ -9,5 +9,7 @@ class Solution:
             for item_s,item_t in zip(set_of_s,set_of_t):
                 if item_s != item_t:
                     return False
-                    return True
-                    return False
+                # If all goes well
+                return True
+        # If lengths ain't t the same
+        return False
