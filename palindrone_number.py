e438f1fd2675ca208edc3dc098c6adcb7f5dfb93
sadatyussuf <tommyleespartan0@gmail.com>
2022-10-30 21:10:11 +0000

    2nd solution to the problem

--- palindrone_number.py
+++ palindrone_number.py
@@ -1,12 +1,25 @@
+# class Solution:
+#     def isPalindrome(self, x: int) -> bool:
+
+#         if x < 0:
+#             return False
+#         if str(x)[::-1] == str(x):
+#             return True
+# return False
+
 class Solution:
     def isPalindrome(self, x: int) -> bool:
 
-        if x < 0:
+        if (x < 0):
             return False
-        if str(x)[::-1] == str(x):
+        reverse_x = 0
+        temp_x = x
+        while x > 0:
+            reverse_x = (reverse_x * 10) + (x % 10)
+            x = x // 10
+        if reverse_x == temp_x:
             return True
-        return False
 
 
 test = Solution()
-test.isPalindrome(121)
+test.isPalindrome(0)
