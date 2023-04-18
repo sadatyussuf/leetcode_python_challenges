0f5fce0200a80eb56afcff6432a8a0521cbed6ba
Sadat Issah <tommyleespartan0@gmail.com>
2023-04-18 11:09:56 +0000

    challenge solved

---
+++ pascal-triangle.py
@@ -0,0 +1,18 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        res = []
+
+        for row in range(numRows):
+            if not res:
+                res.append([1])
+            else:
+                if len(res) == 1:
+                    res.append([1,1])
+                else:
+                    temp_list=[0,*res[-1],0]
+                    cur_list = []
+                    for i in range(len(temp_list)-1):
+                        temp_sum = temp_list[i] + temp_list[i+1]
+                        cur_list.append(temp_sum)
+                    res.append(cur_list)
+        return res
