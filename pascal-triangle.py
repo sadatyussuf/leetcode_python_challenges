d8467058a25ce83a0c900cb0d3fb8f24cb0ca85c
Sadat Issah <tommyleespartan0@gmail.com>
2023-04-18 11:31:20 +0000

    optimized the code

--- pascal-triangle.py
+++ pascal-triangle.py
@@ -1,3 +1,43 @@
+
+# Thrird Optimization
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        res = [[1]]
+
+        for row in range(numRows-1):
+            temp_list=[0,*res[-1],0]  
+            cur_list = list(map(lambda x,y:x+y , 
+                                temp_list[:-1],
+                                temp_list[1:]))
+
+            res.append(cur_list)
+        return res
+
+
+
+
+# Second Try
+
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        res = [[1]]
+
+        for row in range(numRows-1):
+            temp_list=[0,*res[-1],0]
+            cur_list = []     
+            for i in range(len(temp_list)-1):
+                temp_sum = temp_list[i] + temp_list[i+1]
+                cur_list.append(temp_sum)
+            res.append(cur_list)
+        return res
+
+
+
+
+
+
+# First Try 
+
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
         res = []
