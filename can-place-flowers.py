a3614cef93155012e473d30603bad1329f24106b
Sadat Issah <tommyleespartan0@gmail.com>
2023-05-11 12:43:17 +0000

    refactoring the solution

--- can-place-flowers.py
+++ can-place-flowers.py
@@ -1,3 +1,25 @@
+
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        n_of_flower = 0
+
+        for i in range(len(flowerbed)):
+            if (flowerbed[i] == 0 and 
+                (i == 0 or flowerbed[i-1] == 0) and 
+                (i == len(flowerbed)-1 or flowerbed[i+1] == 0)):
+                flowerbed[i] = 1
+                n_of_flower += 1
+                if n_of_flower >= n:
+                    return True
+                # skip the next position
+                i += 1
+
+        return n_of_flower >= n
+
+
+
+
+
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
         n_of_flower = 0
